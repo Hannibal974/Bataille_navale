23a762bbe875a46b8acfacca6d8c3c544477736d
J.droza <jennifer.droza@y-nov.com>
2015-05-22 23:09:02 +0200

    Add a class and modified lobby
    Add class : PlayerIn (extend ArrayList)
    Modified Lobby to implement JComboboxPlayerIn

--- src/bataille_navale/ui/PanelLobby.java
+++ src/bataille_navale/ui/PanelLobby.java
@@ -8,7 +8,7 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
-import random.core.Match;
+//import random.core.Match;
 import bataille_navale.Client;
 import bataille_navale.Player;
 
@@ -21,17 +21,20 @@ public class PanelLobby extends JPanel implements ActionListener
 	private Client client;
 	private Player p;
 	private JButton buttonCreate;
-	private JComboBox<Match> combomatches;
+	//private JComboBox<Match> combomatches;
+	private JComboBox<String> PlayersIn;
 	
 	public PanelLobby (Client cli, Player play)
 	{
 		setLayout(new GridLayout(1,2));
-		combomatches = new JComboBox<Match>();
+		//combomatches = new JComboBox<Match>();
+		PlayersIn = new JComboBox<String>();
 		client = cli;
 		p = play;
 		buttonCreate = new JButton("Create game");
 		buttonCreate.addActionListener(this);
-		add(combomatches);
+		//add(combomatches);
+		add(PlayersIn);
 		add(buttonCreate);
 		setVisible(true);
 	}
