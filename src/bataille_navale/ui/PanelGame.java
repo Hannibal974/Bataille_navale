424ae361b1bd5da2845ed126e1a32db7fcc76a94
J.droza <jennifer.droza@y-nov.com>
2015-05-23 17:56:51 +0200

    Modified games panel
    -Modified aspect PanelConnect
    -Modifed aspect PanelGame
    -Modifie aspect PanelLobby

--- src/bataille_navale/ui/PanelGame.java
+++ src/bataille_navale/ui/PanelGame.java
@@ -1,42 +1,127 @@
 package bataille_navale.ui;
-
+/**
+ *
+ * @author Jennifer & Romain
+ */
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 import bataille_navale.Client;
 
+@SuppressWarnings("serial")
 public class PanelGame extends JPanel implements ActionListener
 {
+	// Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton b_send;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JDialog jd_gameEnd;
+    private javax.swing.JTextArea jta_historique;
+    private javax.swing.JLabel l_Trouverlenombre;
+    private javax.swing.JLabel l_historique;
+    private javax.swing.JLabel ld_resultgame;
+    private javax.swing.JTextField tf_nombre;
+	@SuppressWarnings("unused")
 	private Client client;
-	private JLabel labelnumber;
-	private JTextField textnumber;
-	private JButton buttontosend;
+    // End of variables declaration//GEN-END:variables
 	
 	public PanelGame(Client cli) 
 	{
 		client = cli;
 		setLayout(new GridLayout(1,3));
-		// TODO Auto-generated constructor stub
-		labelnumber = new JLabel("Entrez un nombre");
-		textnumber = new JTextField();
-		buttontosend = new JButton("Envoyer");
-		buttontosend.addActionListener(this);
-		add(labelnumber);
-		add(textnumber);
-		add(buttontosend);
+		
+		initComponents();
 	}
+	
+	private void initComponents() {
+        jToggleButton1 = new javax.swing.JToggleButton();
+        jd_gameEnd = new javax.swing.JDialog();
+        ld_resultgame = new javax.swing.JLabel();
+        l_Trouverlenombre = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jta_historique = new javax.swing.JTextArea();
+        l_historique = new javax.swing.JLabel();
+        tf_nombre = new javax.swing.JTextField();
+        b_send = new javax.swing.JToggleButton();
+
+        jToggleButton1.setText("jToggleButton1");
+
+        ld_resultgame.setText("Résultat du jeu:");
+
+        javax.swing.GroupLayout jd_gameEndLayout = new javax.swing.GroupLayout(jd_gameEnd.getContentPane());
+        jd_gameEnd.getContentPane().setLayout(jd_gameEndLayout);
+        jd_gameEndLayout.setHorizontalGroup(
+            jd_gameEndLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jd_gameEndLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(ld_resultgame)
+                .addContainerGap(313, Short.MAX_VALUE))
+        );
+        jd_gameEndLayout.setVerticalGroup(
+            jd_gameEndLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jd_gameEndLayout.createSequentialGroup()
+                .addComponent(ld_resultgame)
+                .addGap(0, 61, Short.MAX_VALUE))
+        );
+
+        l_Trouverlenombre.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        l_Trouverlenombre.setText("Trouver le nombre");
+
+        jta_historique.setColumns(20);
+        jta_historique.setRows(5);
+        jScrollPane1.setViewportView(jta_historique);
+
+        l_historique.setText("Historique:");
+
+        tf_nombre.setText("Nombre ici");
+
+        b_send.setText("Envoyer");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jScrollPane1)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addComponent(tf_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(b_send, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addComponent(l_historique))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(l_Trouverlenombre, javax.swing.GroupLayout.PREFERRED_SIZE, 253, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(l_Trouverlenombre, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(l_historique)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(b_send, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(tf_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+    }
 	
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
 		// TODO Auto-generated method stub
-		if(e.getSource() == buttontosend)
+		if(e.getSource() == b_send)
 		{
 			
 		}
--- src/bataille_navale/ui/PanelLobby.java
+++ src/bataille_navale/ui/PanelLobby.java
@@ -1,16 +1,13 @@
 package bataille_navale.ui;
-
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+/**
+*
+* @author Jennifer & Romain
+*/
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
-import javax.swing.border.Border;
-
-
 
 //import random.core.Match;
 import bataille_navale.Client;
@@ -18,64 +15,135 @@ import bataille_navale.Player;
 
 public class PanelLobby extends JPanel implements ActionListener
 {
-	/**
-	 * 
-	 */
+	 // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton b_connect;
+    private javax.swing.JButton b_create;
+    private javax.swing.JButton b_deconnect;
+	private javax.swing.JComboBox<String> PlayersIn;
+    private javax.swing.JLabel l_EnterName;
+    private javax.swing.JLabel l_JoinServ;
+    private javax.swing.JLabel l_createParty1;
 	private static final long serialVersionUID = -6003067736193144131L;
 	private Client client;
 	private Player p;
-	private JButton buttonCreate, buttonjoin;
-	//private JComboBox<Match> combomatches;
-	private JComboBox<String> PlayersIn;
 	private MaFrame frame;
+    // End of variables declaration//GEN-END:variables
 	
 	public PanelLobby (Client cli, Player play, String[] listMatchToJoin, MaFrame framish)
 	{
-		setLayout(new GridLayout(1,3));
-		//combomatches = new JComboBox<Match>();
 		PlayersIn = new JComboBox<String>();
 		client = cli;
 		p = play;
 		frame = framish;
-		buttonCreate = new JButton("Create game");
-		buttonCreate.addActionListener(this);
+		 initComponents();
 		
-		//add(combomatches);
 		for(int i = 0; i < listMatchToJoin.length; i++)
 		{
 			//il faut concatÈner la ligne i et i+1 pour avoir le thread et le nom du joueur sur la mÍme ligne
 			PlayersIn.addItem("Playeur: "+listMatchToJoin[i]+"| Match: "+listMatchToJoin[i+1]);
 			i++;//Il faut avancer de 2 lignes du coup 
 		}
-		buttonjoin = new JButton("Rejoindre");
-		buttonjoin.addActionListener(this);
-		add(PlayersIn);
-		add(buttonCreate);
-		add(buttonjoin, BorderLayout.SOUTH);
 		setVisible(true);
 	}
 
+	 private void initComponents() {
+
+	        l_JoinServ = new javax.swing.JLabel();
+	        l_EnterName = new javax.swing.JLabel();
+	        b_connect = new javax.swing.JButton();
+	        PlayersIn = new JComboBox<String>();
+	        b_create = new javax.swing.JButton();
+	        l_createParty1 = new javax.swing.JLabel();
+	        b_deconnect = new javax.swing.JButton();
+
+	        l_JoinServ.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+	        l_JoinServ.setText("Rejoindre une partie");
+
+	        l_EnterName.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+	        l_EnterName.setText("Selectionner votre adversaire");
+
+	        b_connect.setText("Cr√©er une partie");
+
+	        PlayersIn.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+
+	        b_create.setText("Se connecter");
+
+	        l_createParty1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+	        l_createParty1.setText("Ou cr√©er une partie:");
+
+	        b_deconnect.setText("Se d√©connecter");
+
+	        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+	        this.setLayout(layout);
+	        layout.setHorizontalGroup(
+	            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	            .addGroup(layout.createSequentialGroup()
+	                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	                            .addGroup(layout.createSequentialGroup()
+	                                .addGap(130, 130, 130)
+	                                .addComponent(l_JoinServ))
+	                            .addGroup(layout.createSequentialGroup()
+	                                .addContainerGap()
+	                                .addComponent(l_EnterName))
+	                            .addGroup(layout.createSequentialGroup()
+	                                .addContainerGap()
+	                                .addComponent( PlayersIn, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE)
+	                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+	                                .addComponent(b_create, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE))
+	                            .addGroup(layout.createSequentialGroup()
+	                                .addContainerGap()
+	                                .addComponent(l_createParty1)))
+	                        .addGap(0, 4, Short.MAX_VALUE))
+	                    .addGroup(layout.createSequentialGroup()
+	                        .addContainerGap()
+	                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	                            .addComponent(b_connect, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+	                            .addComponent(b_deconnect, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+	                .addContainerGap())
+	        );
+	        layout.setVerticalGroup(
+	            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+	            .addGroup(layout.createSequentialGroup()
+	                .addContainerGap()
+	                .addComponent(l_JoinServ)
+	                .addGap(25, 25, 25)
+	                .addComponent(l_EnterName)
+	                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+	                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+	                    .addComponent( PlayersIn, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+	                    .addComponent(b_create, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE))
+	                .addGap(33, 33, 33)
+	                .addComponent(l_createParty1)
+	                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+	                .addComponent(b_connect, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+	                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+	                .addComponent(b_deconnect)
+	                .addContainerGap())
+	        );
+	    }
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
 		// TODO Auto-generated method stub
-		if(e.getSource() == buttonCreate)
+		if(e.getSource() == b_create)
 		{
 			String validation = client.CreateMatch(p);
 			System.out.println(validation);
 		}
-		else if (e.getSource() == buttonjoin)
+		else if (e.getSource() == b_connect)
 		{
 			String validation = client.JoinMatch(PlayersIn.getSelectedIndex(), p);
 			System.out.println(validation);
 		}
+		
 		frame.getContentPane().setVisible(false);
 		frame.getContentPane().remove(this);
-		///////////////////////////////////////////////////////////
-		// Je ne sais pas quoi passer en paramËtres du PanelGame
+		//Lancement du panel
 		frame.getContentPane().add(new PanelGame(client));
-		//
-		///////////////////////////////////////////////////////////
 		frame.getContentPane().setVisible(true);
 	}
 	
