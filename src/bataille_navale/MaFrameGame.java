e1d60062fad9f3addce8075957253ce893140672
Romain Poussin <romain.poussin@y-nov.com>
2015-05-24 16:49:49 +0200

    gestion erreur jtextfield

--- src/bataille_navale/MaFrameGame.java
+++ src/bataille_navale/MaFrameGame.java
@@ -1,6 +1,9 @@
 package bataille_navale;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
+
 import bataille_navale.ui.PanelGame;
 
 @SuppressWarnings("serial")
@@ -13,13 +16,12 @@ public class MaFrameGame extends JFrame
 	{
 		setTitle("serveur de game");
 		setSize(900, 600);
+		setLayout(new GridLayout(1, 1));
 		client = cli;
-		//Lancement du panel
+		//Lancement du panel1
 		panelgame = new PanelGame(client);
-		panelgame.setVisible(true);
 		System.out.println("coucou la frame game");
 		add(panelgame);
 		System.out.println("Affiche frame Game");
-		setVisible(true);
 	}
 }
